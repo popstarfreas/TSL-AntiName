7e8a440f73e8deb4734862892ddc0d8000b42d37
popstarfreas <popstarfreas2@gmail.com>
2025-07-11 10:36:23 +0100

    chore: Update to pnpm

--- build.plugin.js
+++ build.plugin.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var child_process = require("child_process");
 
-child_process.execSync("npm i ../../pluginreference", { stdio: "inherit" })
-child_process.execSync("npm run build", { stdio: "inherit" })
-child_process.execSync("npm run build:packed", { stdio: "inherit" })
+child_process.execSync("pnpm i ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("pnpm run build", { stdio: "inherit" })
+child_process.execSync("pnpm run build:packed", { stdio: "inherit" })
 fs.renameSync("./output", "./plugin")
