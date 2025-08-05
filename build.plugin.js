8dcb38b94d8ee02ffa77fe901088815a1c501627
popstarfreas <popstarfreas2@gmail.com>
2025-08-05 23:15:37 +0100

    build: Fix pnpm failure

--- build.plugin.js
+++ build.plugin.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var child_process = require("child_process");
 
-child_process.execSync("pnpm i ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("pnpm i --production=true ../../pluginreference", { stdio: "inherit" })
 child_process.execSync("pnpm run build", { stdio: "inherit" })
 child_process.execSync("pnpm run build:packed", { stdio: "inherit" })
 fs.renameSync("./output", "./plugin")
